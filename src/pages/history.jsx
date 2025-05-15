07eff7c360c28b92c2702ad590fd756c3ffad939
Jacobo1410 <jacobro2006@gmail.com>
2025-05-15 07:20:15 -0500

    Update history.jsx

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -3,7 +3,7 @@ import Title from '../pages/titles.jsx';
 const History = () => {
     return (
         <div className="container py-5">
-            <Title text = {"Home"} />
+            <Title text = {"Historia"} />
                 <div className = "col-md-9">
                     <p>Fundados en 1946, los Boston Celtics son uno de los equipos más antiguos y laureados de la NBA. Con sede en Boston, Massachusetts, han ganado 17 campeonatos de la NBA, compartiendo el récord de títulos con los Lakers. El legado de los Celtics está marcado por nombres legendarios como Red Auerbach, Bill Russell, Larry Bird, y más recientemente, figuras como Paul Pierce y Kevin Garnett, quienes lideraron la conquista del título en 2008. Más que un equipo, los Celtics son un símbolo de lucha, estrategia y orgullo. Su uniforme verde y su trebol característico han dejado huella en generaciones de fanáticos y en la historia del baloncesto mundial.
                     </p>
